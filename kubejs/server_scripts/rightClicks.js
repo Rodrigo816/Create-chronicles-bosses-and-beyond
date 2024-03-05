275a620e865853e49ffcce494016f6059a9fa9c7
Rodrigo Borba <rodrigo14.84@gmail.com>
2024-03-05 00:23:30 -0100

    added talent tree

--- kubejs/server_scripts/rightClicks.js
+++ kubejs/server_scripts/rightClicks.js
@@ -1,7 +1,7 @@
 ItemEvents.rightClicked( event => {
     if(event.item=='kubejs:skill_add_point') {
         // Run the skill point addition command for the player who clicked the block
-        event.server.runCommandSilent(`/puffish_skills points add ${event.player.username} main 1`);
+        event.server.runCommandSilent(`/puffish_skills points add ${event.player.username} talentree 1`);
         
         // If the player is holding an item, reduce its count by 1
         if(event.item) {
@@ -13,7 +13,7 @@ ItemEvents.rightClicked( event => {
     }
     if(event.item=='kubejs:skill_reset') {
         // Run the skill point addition command for the player who clicked the block
-        event.server.runCommandSilent(`/puffish_skills skills reset ${event.player.username} main`);
+        event.server.runCommandSilent(`/puffish_skills skills reset ${event.player.username} talentree`);
         
         event.player.tell(Text.gold('Your talent tree was been reseted!'));
         // If the player is holding an item, reduce its count by 1
