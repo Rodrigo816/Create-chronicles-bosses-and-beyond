906ab8b5a78f1827b679fd3489d720055c40a748
Rodrigo Borba <rodrigo14.84@gmail.com>
2024-05-22 20:00:27 +0000

    release 1.6.0

--- generateServerFiles.ps1
+++ generateServerFiles.ps1
@@ -1,6 +1,6 @@
 ### Variables
 # Mod blacklist to NOT copy into serve side folder
-$MOD_BLACK_LIST="betterfpsdist*", "MapTooltip-forge*", "probejs*", "moreoverlays*", "MouseTweaks*", "oculus*", "EquipmentCompare*", "Prism*", "Boat-Item-View*", "tia-1.20*", "gpumemleakfix*", "LegendaryTooltips*", "Highlighter*", "Ding*", "AmbientSounds*", "modnametooltip*", "fancymenu*", "SimpleDiscordRichPresence*", "OverflowingBars*","ToolStats-Forge-*","AmbientSounds_FORGE*","SimpleDiscordRichPresence-forge-*","fancymenu_forge_*", "drippyloadingscreen_forge_*","melody_forge_*"
+$MOD_BLACK_LIST="betterfpsdist*","BHMenu*","cherishedworlds-forge*", "MapTooltip-forge*", "probejs*", "moreoverlays*", "MouseTweaks*", "oculus*", "EquipmentCompare*", "Prism*", "Boat-Item-View*", "tia-1.20*", "gpumemleakfix*", "LegendaryTooltips*", "Highlighter*", "Ding*", "AmbientSounds*", "modnametooltip*", "fancymenu*", "SimpleDiscordRichPresence*", "OverflowingBars*","ToolStats-Forge-*","AmbientSounds_FORGE*","SimpleDiscordRichPresence-forge-*","fancymenu_forge_*", "drippyloadingscreen_forge_*","melody_forge_*"
 
 # Folder list to copy
 $FOLDER_LIST= @("mods", "kubejs", "config", "defaultconfigs")
